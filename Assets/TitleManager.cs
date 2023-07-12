b74cc00569f16525ab1d074da47b34df7ee298a4
shotan1117 <131416539+shotan1117@users.noreply.github.com>
2023-07-12 09:28:42 +0900

    Titleのマネージャー作成

--- Assets/TitleManager.cs
+++ Assets/TitleManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class TitleManager : MonoBehaviour
 {
+    Button button;
+    public GameObject title;
     // Start is called before the first frame update
     void Start()
     {
-        
+        button = GetComponent<Button>();
     }
 
     // Update is called once per frame
