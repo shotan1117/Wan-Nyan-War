3492f8e2c3bd806667696d74d1da58dbe8209569
shotan1117 <131416539+shotan1117@users.noreply.github.com>
2023-07-20 17:25:41 +0900

    リザルトのSE追加

--- Assets/_cs/ResultManager.cs
+++ Assets/_cs/ResultManager.cs
@@ -7,8 +7,8 @@ using UnityEngine.SceneManagement;
 
 public class ResultManager : MonoBehaviour
 {
-    int p1_score = 0;
-    int p2_score = 0;
+    public int p1_score = 0;
+    public int p2_score = 0;
     int timeCnt = 0;
     //int cnt = 0;
     Text txt_point;
@@ -18,6 +18,11 @@ public class ResultManager : MonoBehaviour
     Text txt_winner_Player;
     Text txt_1p_point;
     Text txt_2p_point;
+    AudioSource audioSource;
+    public AudioClip nyan1;
+    public AudioClip nyan2;
+    public AudioClip nyan3;
+    public AudioClip nyan4;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +35,8 @@ public class ResultManager : MonoBehaviour
         txt_winner_Player = GameObject.Find("Canvas").transform.Find("Winner_Player").GetComponent<Text>();
         txt_1p_point = GameObject.Find("Canvas").transform.Find("1P_Point").GetComponent<Text>();
         txt_2p_point = GameObject.Find("Canvas").transform.Find("2P_Point").GetComponent<Text>();
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -38,32 +45,38 @@ public class ResultManager : MonoBehaviour
         this.timeCnt++;
         if(this.timeCnt == 30)
         {
+            audioSource.PlayOneShot(nyan4);
             txt_point.text = "得点は・・・";
         }
         if(this.timeCnt == 90)
         {
+            audioSource.PlayOneShot(nyan1);
             txt_1P.text = "1P";
             txt_2P.text = "2P";
         }
         if(this.timeCnt ==  150)
         {
+            audioSource.PlayOneShot(nyan2);
             txt_1p_point.text = p1_score.ToString();
             txt_2p_point.text = p2_score.ToString();
         }
         if(this.timeCnt == 210)
         {
-            if(p1_score > p2_score)
+            if (p1_score > p2_score)
             {
+                audioSource.PlayOneShot(nyan1);
                 txt_winner.text = "勝者は・・・";
                 txt_winner_Player.text = "1P";
             }
             else if(p1_score < p2_score)
             {
+                audioSource.PlayOneShot(nyan2);
                 txt_winner.text = "勝者は・・・";
                 txt_winner_Player.text = "2P";
             }
             else
             {
+                audioSource.PlayOneShot(nyan3);
                 txt_winner.text = "引き分け";
                 txt_winner_Player.text = "";
             }
