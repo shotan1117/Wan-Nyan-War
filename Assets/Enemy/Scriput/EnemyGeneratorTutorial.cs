b0c97b20deab9f2175c5c22a225768c565fc904e
CI0403 <22ci0403@jec.ac.jp>
2023-07-26 10:43:22 +0900

    チュートリアル（敵）

--- Assets/Enemy/Scriput/EnemyGeneratorTutorial.cs
+++ Assets/Enemy/Scriput/EnemyGeneratorTutorial.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyGeneratorTutorial : MonoBehaviour
 {
     public GameObject DogPBR;
-    private float span = 6.0f;
+    private float span = 4.0f;
     private float delta = 0;
     private float timeCnt = 0;
 
@@ -25,17 +25,17 @@ public class EnemyGeneratorTutorial : MonoBehaviour
 
             if (this.timeCnt > 30)
             {
-                this.span = 4.5f;
+                this.span = 2.5f;
             }
             else if (this.timeCnt > 60)
             {
-                this.span = 2.0f;
+                this.span = 1.5f;
             }
             if (this.delta > this.span)
             {
                 this.delta = 0;
-                float x = Random.Range(-6.0f, 12.0f);
-                float z = Random.Range(-16.0f, -19.0f);
+                float x = Random.Range(-1.5f, 1.5f);
+                float z = Random.Range(-1.5f, 1.5f);
                 Instantiate(DogPBR, new Vector3(x, 0.5f, z), DogPBR.transform.rotation);
             }
         }
