42bb9ce59b22b892e4aabb9ab4da008ddf490267
22ci0401 <22ci0401@jec.ac.jp>
2023-07-26 10:00:02 +0900

    チュートリアルのプレイヤー実装

--- Assets/player/playerShot.cs
+++ Assets/player/playerShot.cs
@@ -9,32 +9,18 @@ public class playerShot : MonoBehaviour
    private int playerNo;
     public GameObject Shot;
     private bool hitchack;
-    private float shotcount;
-    Rigidbody shotRb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        shotRb = Shot.GetComponent<Rigidbody>();
-    }
 
     // Update is called once per frame
     void Update()
     {
-      
-        shotcount+=Time.deltaTime;
         hitchack = player_invincible.invincibleCkack;
       //  if (!hitchack)
         {
-           // if(shotcount  ==5)
-            {
-
                 string buttonName = "RB" + playerNo;
                 if (Input.GetButtonDown(buttonName))
                 {
                         Instantiate(Shot, transform.position, Quaternion.identity);
-                   
                 }
-            }
         }
     }
 }
