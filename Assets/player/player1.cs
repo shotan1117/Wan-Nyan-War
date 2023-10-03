6eb00679fc1fd7ad9c46aae453a96e5271260f1e
22ci0401 <22ci0401@jec.ac.jp>
2023-10-03 12:03:58 +0900

    プレイヤー移動調整

--- Assets/player/player1.cs
+++ Assets/player/player1.cs
@@ -17,16 +17,12 @@ public class player1 : MonoBehaviour
     private float playersiderotate;
 
     public GameObject cam;
-    Quaternion cameraRot, characterRot;
+    Quaternion characterRot;
     private Rigidbody rb;
     private Vector2 move;
 
-    //変数の宣言(角度の制限用)
-    float minX = -90f, maxX = 90f;
-
     void Start()
     {
-        cameraRot = cam.transform.localRotation;
         characterRot = transform.localRotation;
         rb = GetComponent<Rigidbody>();
     }
@@ -35,40 +31,15 @@ public class player1 : MonoBehaviour
     void Update()
     {
         float xRot = Input.GetAxis("direction" + playerNo) * playersiderotate;
-        //縦方向の視点変更は遅く
-        //float yRot = Input.GetAxis("directions" + playerNO) * playersiderotate * 0.5f;
         move.x = Input.GetAxis("Horizontal" + playerNo) * speed;
         move.y = Input.GetAxis("Vertical" + playerNo) * speed;
 
-        // cameraRot *= Quaternion.Euler(-yRot, 0, 0);
         characterRot *= Quaternion.Euler(0, xRot, 0);
-
-        //Updateの中で作成した関数を呼ぶ
-        //cameraRot = ClampRotation(cameraRot);
-        //cam.transform.localRotation = cameraRot;
         transform.localRotation = characterRot;
     }
 
     private void FixedUpdate()
     {
         rb.velocity = (transform.right * move.x) + (transform.forward * move.y) + new Vector3(0, rb.velocity.y, 0);
-    }
-
-    //角度制限関数の作成
-    private Quaternion ClampRotation(Quaternion q)
-    {
-        //q = x,y,z,w (x,y,zはベクトル（量と向き）：wはスカラー（座標とは無関係の量）)
-        q.x /= q.w;
-        q.y /= q.w;
-        q.z /= q.w;
-        q.w = 1f;
-
-        float angleX = Mathf.Atan(q.x) * Mathf.Rad2Deg * 2f;
-
-        angleX = Mathf.Clamp(angleX, minX, maxX);
-
-        q.x = Mathf.Tan(angleX * Mathf.Deg2Rad * 0.5f);
-
-        return q;
     }
 }
