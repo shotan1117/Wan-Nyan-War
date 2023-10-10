2743cd479aaea730f515f6f4d3ed1be3f0967762
22ci0401 <22ci0401@jec.ac.jp>
2023-10-10 12:05:15 +0900

    プレイヤー調整

--- Assets/_cs/coin fly.cs
+++ Assets/_cs/coin fly.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class coinfly : MonoBehaviour
 {
+    [SerializeField]
+    float CoinStopY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +15,20 @@ public class coinfly : MonoBehaviour
         int z = Random.Range(0, 250);
      
         GetComponent<Rigidbody>().AddForce(new Vector3(x,y, z));
-        
+        StartCoroutine(CheckPositionY());
     }
+
+    IEnumerator CheckPositionY()
+    {
+        while(transform.position.y > CoinStopY)
+        {
+            yield return null;
+        }
+
+        var rb = GetComponent<Rigidbody>();
+        Destroy(rb);
+    }
+
 
     // Update is called once per frame
     void Update()
--- Assets/player/player_invincible.cs
+++ Assets/player/player_invincible.cs
@@ -10,21 +10,58 @@ public class player_invincible : MonoBehaviour
     private float BlinkDuration;
     float blinkTime;
     private Renderer[] mrList;
+    public GameObject coin;
+    private int coinGenerate;
 
     [SerializeField]
-    private float playerNo;
+    private int playerNo;
     void Start()
     {
          mrList = GetComponentsInChildren<Renderer>();
     }
 
-    public void Damage()
+    private void Damage()
     {
         elapsedTime = 0;
         blinkTime = 0;
         invincibleCkack = true;
+        CoinGenerate();
     }
-
+    void CoinGenerate()
+    {
+        if (playerNo == 1)
+        {
+            
+            if (ScoreManager.GetP1Score() > 3)
+            {
+                coinGenerate = 3;
+            }
+            else
+            {
+                coinGenerate = ScoreManager.GetP1Score();
+            }
+            ScoreManager.MinusP1Score(3);
+        }
+        else if (playerNo == 2)
+        {
+           
+            if (ScoreManager.GetP2Score() > 3)
+            {
+                coinGenerate = 3;
+            }
+            else
+            {
+                coinGenerate = ScoreManager.GetP2Score();
+            }
+            ScoreManager.MinusP2Score(3);
+        }
+        for (int i = 0; i < coinGenerate; i++)
+        {
+            Vector3 v = transform.position/* + transform.up * 5*/;
+            GameObject coinn = Instantiate(coin, v, Quaternion.identity);
+            coinn.transform.Rotate(new Vector3(67.941f, 188.771f, 0.638f));
+        }
+    }
     void InvincibleChange(bool flag)
     {
         foreach (var mr in mrList)
@@ -58,37 +95,31 @@ public class player_invincible : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "enemy")
+        if (other.gameObject.tag == "enemy" && invincibleCkack ==false)
         {            
-                Damage();
+            Damage();
         }
-
-        if (other.gameObject.tag == "Coin" && !invincibleCkack)
+        
+        if (other.gameObject.tag == "Shot" && invincibleCkack == false)
         {
-            Debug.Log("a");
+            Damage();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin" && invincibleCkack == false)
+        {
             if (playerNo == 1)
             {
-                Debug.Log("1");
                 ScoreManager.AddP1Score(1);
             }
             else if (playerNo == 2)
             {
-                Debug.Log("2");
+
                 ScoreManager.AddP2Score(1);
             }
             Destroy(other.gameObject);
-        }
-
-        if (other.gameObject.tag == "Shot")
-        {
-            if (playerNo == 1)
-            {
-                ScoreManager.MinusP1Score(1);
-            }
-            else if (playerNo == 2)
-            {
-                ScoreManager.MinusP2Score(1);
-            }
         }
     }
 }
