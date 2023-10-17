81b6a5722d9ed64abfef7e961e36fa62e3de8f84
0322 <test@jec.ac.jp>
2023-10-17 11:16:10 +0900

    爆弾

--- Assets/bomb activate.cs
+++ Assets/bomb activate.cs
@@ -2,20 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bombstartspeed : MonoBehaviour
+public class bombactivate : MonoBehaviour
 {
-    Rigidbody rb;
-
+     public GameObject bomb;
     // Start is called before the first frame update
     void Start()
     {
-        rb=GetComponent<Rigidbody>();
-        rb.velocity = new Vector3 (0,5,5);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        bomb.GetComponent<BoxCollider>().enabled = true;
         
     }
 }
