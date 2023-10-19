f9f29705777648789d90c640d7462df8ea00d536
shotan1117 <131416539+shotan1117@users.noreply.github.com>
2023-10-19 11:06:09 +0900

    タイトルの変更

--- Assets/_cs/TitleManager.cs
+++ Assets/_cs/TitleManager.cs
@@ -7,27 +7,18 @@ using Unity.VisualScripting;
 
 public class TitleManager : MonoBehaviour
 {
-    Button button;
     public GameObject title;
 
     public GameObject tutorial;
-    public GameObject game;
-
-    private double time_;
-   
+    public GameObject game;   
     // Start is called before the first frame update
     void Start()
     {
-        button = GetComponent<Button>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        time_ += Time.deltaTime;
-
-
-
         if (Input.GetButtonDown("Start"))
         {
             SceneManager.LoadScene("Game");
