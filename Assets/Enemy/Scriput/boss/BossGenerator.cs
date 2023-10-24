b7f36f86d924016d95746e7ba5bd4773059ebef1
shotan1117 <131416539+shotan1117@users.noreply.github.com>
2023-10-24 09:58:58 +0900

    リベース＆衝突解消

--- Assets/Enemy/Scriput/boss/BossGenerator.cs
+++ Assets/Enemy/Scriput/boss/BossGenerator.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<<< HEAD:Assets/Enemy/Scriput/boss/BossGenerator.cs
 public class BossGenerator : MonoBehaviour
 {
 
@@ -9,6 +10,14 @@ public class BossGenerator : MonoBehaviour
     void Start()
     {
         
+========
+public class smokedestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, 5f);
+>>>>>>>> 2fa96d62 (爆弾):Assets/VFX/smoke destroy.cs
     }
 
     // Update is called once per frame
