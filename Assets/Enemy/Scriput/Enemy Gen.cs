5cedbc9d267a828518cd94cd242919d21341fa53
shotan1117 <131416539+shotan1117@users.noreply.github.com>
2023-10-24 09:34:54 +0900

    リベース＆衝突解消

--- Assets/Enemy/Scriput/Enemy Gen.cs
+++ Assets/Enemy/Scriput/Enemy Gen.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
 
 public class EnemyGen : MonoBehaviour
 {
@@ -44,6 +45,10 @@ public class EnemyGen : MonoBehaviour
 
     public float enemyCreateRadius = 8;
 
+    GameObject image;
+
+    public GameObject canvas;
+
     private void Start()
     {
         GD = GameObject.Find("gameDirector");
@@ -64,6 +69,8 @@ public class EnemyGen : MonoBehaviour
         StartCoroutine(CoBossCreate(0, gameDirector.wave1 / 2, 5, positions[0]));
         StartCoroutine(CoBossCreate(gameDirector.wave1, gameDirector.wave2 / 2, 5, positions[1]));
         StartCoroutine(CoBossCreate(gameDirector.wave2 + gameDirector.wave1, gameDirector.wave3 / 2, 5, positions[2]));
+
+        //警告画像の初期化
     }
 
     IEnumerator CoEnemyCreate(float delay, float appearTime, float stopTime, Transform createPos)
@@ -89,7 +96,15 @@ public class EnemyGen : MonoBehaviour
                 float z = createPos.position.z + Mathf.Sin(rad) * dist;
 
                 // GameObjectを上記で決まったランダムな場所に生成
+
                 Instantiate(Enemy, new Vector3(x, y, z), Enemy.transform.rotation);
+
+                if (GameObject.Find("Logo1(Clone)") == null)
+                {
+                    GhostArrangement("Logo1", -1500, 300);
+                    GhostArrangement("Logo2", -1500, 150);
+                }
+               
             }
 
             yield return null;
@@ -119,11 +134,53 @@ public class EnemyGen : MonoBehaviour
         }
         yield return null;
     }
-    
 
-    // Update is called once per frame
+
+    public void GhostArrangement(string prefabs_path, float pos_x, float pos_y)
+    {
+        //インスタンスの生成
+        GameObject ui = (GameObject)Resources.Load(prefabs_path);
+
+        /*{ 
+        //修正前
+        Instantiate(ui, new Vector3(pos_x, pos_y, 0), Quaternion.identity);
+
+        //canvasの子に指定
+        ui.transform.SetParent(this.canvas.transform, false);
+         }*/
+
+        //修正後
+        GameObject ui_clone = Instantiate(ui, new Vector3(pos_x, pos_y, 0), Quaternion.identity);
+
+        //canvasの子に指定
+        ui_clone.transform.SetParent(canvas.transform, false);
+    }
+        // Update is called once per frame
     void Update()
     {
-     
+         if(GameObject.Find("Logo1(Clone)") != null)
+         {
+            GameObject logo1 = GameObject.Find("Logo1(Clone)");
+            if (logo1.transform.position.x <= 3000)
+            {
+                logo1.transform.position += new Vector3(10, 0, 0);
+            }
+            else
+            {
+                Destroy(logo1);
+            }
+         }
+        if (GameObject.Find("Logo2(Clone)") != null)
+        {
+            GameObject logo2 = GameObject.Find("Logo2(Clone)");
+            if (logo2.transform.position.x <= 3000)
+            {
+                logo2.transform.position += new Vector3(10, 0, 0);
+            }
+            else
+            {
+                Destroy(logo2);
+            }
+        }
     }
 }
